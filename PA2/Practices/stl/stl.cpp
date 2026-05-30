84028f93131d1ed6d3d8a33075a8d1603a383e55
Marwan Ahmed <marwanahmed09763@gmail.com>
2026-05-30 22:56:33 +0200

    Refactor and enhance STL functions for improved functionality and readability

--- PA2/Practices/stl/stl.cpp
+++ PA2/Practices/stl/stl.cpp
@@ -1,12 +1,16 @@
 // sum input numbers
 #include <iostream>
-
-
-
+#include <numeric>
+#include <iterator>
+#include <vector>
+#include <algorithm>
+#include <set>
+#include <map>
 
 using namespace std;
 
-int main()
+
+void oldSum()
 {
     int sum = 0,x;
     while(cin>>x)
@@ -16,9 +20,77 @@ int main()
         sum+=x;
     }
     cout<<sum<<endl;
+}
 
-
+void newSum()
+{
     // iterator and generic function
+    int newSum = accumulate ( istream_iterator<int> (cin) , istream_iterator<int> (),0);
+    cout<<newSum<<endl;
+}
+
+int multiply(int a, int b)
+{
+    return a*b;
+} 
+
+void accumulateorMulti()
+{
+    int x = accumulate(istream_iterator<int>(cin) ,istream_iterator<int> (),1,multiply);
+    cout<<x<<endl;
+}
+
+// average = sum / count 
+// lets get sum and conut 
+pair<int,int> avg(const pair<int,int>& a, int b)
+{
+    return make_pair(a.first + b, a.second + 1);
+}
+
+void printAvg()
+{
+    pair<int,int> x = accumulate
+    (
+        istream_iterator<int> (cin),
+        istream_iterator<int> (),
+        make_pair(0,0),
+        avg
+    );
+
+    cout<< x.first / x.second <<endl;
+}
+
+int maximum(int a, int b)
+{
+    return max(a,b);
+}
+
+int main()
+{
+    
+    
+
+    // Count frequency of words.
+    // Input: cat dog cat cat dog
+    // Output:
+    // cat -> 3
+    // dog -> 2
+    map<string,int> freq;
+
+    string word;
+    while(cin>>word)
+    {
+        freq[word]++;
+    }
+    for(auto p : freq)
+    {
+        cout <<
+        p.first
+        <<"->"
+        <<p.second
+        <<endl;
+    }
+
 
     return 0;
 }
